78e8f9ab7214d661bc3822cc7e47a83a8937d253
leo233 <leo233.huang@gmail.com>
2025-04-24 21:33:09 +0800

    第一个集成测试pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use actix_web::dev::Server;
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+// pub async fn run() -> std::io::Result<()> {
+// 注意这里删除了async, 也没有await了
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
@@ -9,8 +12,9 @@ pub async fn run() -> std::io::Result<()> {
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+
+    Ok(server)
 }
 
 async fn greet(req: HttpRequest) -> impl Responder {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,10 +4,11 @@ use tokio;
 // 可以使用 cargo +nightly expand --test health_check 查看展开后的代码
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn app");
+    // spawn_app().await.expect("Failed to spawn app");
+    spawn_app(); // 不需要await, expect了
     let client = reqwest::Client::new();
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get("http://localhost:8000/health_check")
         .send()
         .await
         .expect("Failed to send request");
@@ -16,6 +17,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
+// async fn spawn_app() -> std::io::Result<()> {
+fn spawn_app() {
+    let server = zero2prod::run().expect("failed to bind addr");
+    // 启动服务器作为后台任务
+    // tokio::spawn 返回一个指向spawned future的handle
+    let _handle = tokio::spawn(server);
 }
