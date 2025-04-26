dd9f05f9e89a31c57457bfed8aa9d4b71108f1dc
leo <leo233.huang@gmail.com>
2025-04-26 18:41:09 +0800

    add subscripition and test case

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // 为 POST/subscriptions 在请求路由表里添加一个新的条目
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/", web::get().to(greet))
             // 这个要么删除，要么放到最后，因为App会遍历所有注册的端点，放在前面会被优先匹配
             .route("/{name}", web::get().to(greet))
@@ -25,6 +27,16 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 // async fn health_check(req: HttpRequest) -> impl Responder {
 async fn health_check() -> impl Responder {
-    // HttpResponse::Ok().finish()
-    HttpResponse::Ok()
+    //和直接用Ok()返回是一样的效果
+    //为了和其他的风格一致,使用finish()
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -32,3 +32,49 @@ fn spawn_app() -> String {
     let _handle = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_valid_from_data() {
+    //准备
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    //执行
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    //准备
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        //("name=le%20guin&email=ursula_le_guin%40gmail.com")
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    //执行
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API didn't fail with 400 Bad Request when payload was {}",
+            error_message
+        );
+    }
+}
